81cdf0a0bb2777e0e303b61696c92abd4bb24240
Linda Albrektsson <linda.albrektsson@gmail.com>
2026-09-22 19:01:03 +0200

    Styled the drawer and added a Close-icon.

--- src/components/CartDrawer/CartDrawer.tsx
+++ src/components/CartDrawer/CartDrawer.tsx
@@ -1,4 +1,7 @@
 import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import "./CartDrawer.css";
 
 interface CartDrawerProps {
   open: boolean;
@@ -8,7 +11,15 @@ interface CartDrawerProps {
 function CartDrawer({ open, onClose }: CartDrawerProps) {
   return (
     <Drawer anchor="right" open={open} onClose={onClose}>
-      <div>YOUR CART</div>
+      <div className="cart-drawer">
+        <div className="cart-drawer__header">
+          <h2>YOUR CART</h2>
+
+          <IconButton aria-label="close cart" onClick={onClose}>
+            <CloseIcon />
+          </IconButton>
+        </div>
+      </div>
     </Drawer>
   );
 }
